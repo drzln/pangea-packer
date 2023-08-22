0b1efb95a6e3c7ecf463e04dbee00c1b79a670b0
luis <ldesiqueira@pinger.com>
2023-08-22 16:15:16 -0300

    pangea packer

--- spec/infra_spec.rb
+++ spec/infra_spec.rb
@@ -2,4 +2,12 @@
 # create test infrastructure with pangea
 ###############################################################################
 
+require %(terraform-synthesizer)
+
+describe %(infrastructure state) do
+  let(:tf) do
+    TerraformSynthesizer.new
+  end
+end
+
 # end create test infrastructure with pangea
