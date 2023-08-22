b1bd2ba1fe791af1b10e9f874724f2bc33e03730
luis <ldesiqueira@pinger.com>
2023-08-22 12:15:58 -0300

    pangea packer

--- lib/pangea-packer.rb
+++ lib/pangea-packer.rb
@@ -1,10 +1,9 @@
 require %(json)
-# require %(pangea_component/pangea_component)
-# require_relative %(/Users/ldesiqueira/.rbenv/versions/3.1.0/gemsets/pangea-packer/gems/pangea-component-0.0.1/src/lib/pangea_component/pangea_component.rb)
+require %(pangea-component)
 
-# component resource: :aws_vpc,
-#           name: :packer,
-#           properties: {
-#             cidr_block: %(10.1.0.0/16),
-#             tags: { Name: :packer }
-#           }
+component resource: :aws_vpc,
+          name: :packer,
+          properties: {
+            cidr_block: %(10.1.0.0/16),
+            tags: { Name: :packer }
+          }
---
+++ lib/version.rb
@@ -0,0 +1,3 @@
+module PangeaPacker
+  VERSION = %(0.0.1).freeze
+end
