ee29b73856719b999250af823f570823a164dd80
luis <ldesiqueira@pinger.com>
2023-08-22 16:42:53 -0300

    init

--- spec/infra_spec.rb
+++ spec/infra_spec.rb
@@ -4,6 +4,15 @@
 
 require %(terraform-synthesizer)
 require %(pangea)
+require %(ostruct)
+
+def deep_ostruct(hash)
+  OpenStruct.new(
+    hash.each_with_object({}) do |(key, value), result|
+      result[key] = value.is_a?(Hash) ? deep_ostruct(value) : value
+    end
+  )
+end
 
 describe %(infrastructure state) do
   let(:tf) do
@@ -14,8 +23,20 @@ describe %(infrastructure state) do
     Pangea
   end
 
+  let(:config) do
+    deep_ostruct({
+      # directory where we store pangea configuration
+      # and other assets needed to setup test
+      test: { home: %(./spec/test_home/infrastructure_state) }
+    })
+  end
+
   it %(passes the module test) do
     expect(pangea.ping).to be_a(String)
+  end
+
+  it %(creates config ostruct) do
+    expect(config.test.home).to be_a(String)
   end
 end
 
